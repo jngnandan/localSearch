996e014f8ac787296d97ca751e28596b5e979735
jngnandan <jngnandan@gmail.com>
2023-08-16 02:07:45 +0100

    initial

---
+++ src/components/Home.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className='bg-purple-800 h-screen flex flex-col justify-center items-center h-screen'>
+
+      <div className='flex flex-wrap justify-center items-center'>
+      <h1 className='w-full text-3xl text-white my-8'>Super over league</h1>
+
+        <div>
+          <img src='https://assets.ccbp.in/frontend/react-js/rcb-img.png'/>
+        </div>
+        <div>
+        <img src='https://assets.ccbp.in/frontend/react-js/csk-img.png'/>
+        </div>
+        </div>
+    </div>
+  )
+}
+
+
+
+// https://assets.ccbp.in/frontend/react-js/rcb-img.png
+// https://assets.ccbp.in/frontend/react-js/csk-img.png
